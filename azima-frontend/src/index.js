67ce36ee35d85dd7fa00058ad185152b985c39ca
SylvCrane <104751218+SylvCrane@users.noreply.github.com>
2023-08-29 13:59:53 +1200

    Added react compatability

--- azima-frontend/src/index.js
+++ azima-frontend/src/index.js
@@ -1,17 +1,50 @@
+import 'aframe';
+import {Entity, Scene} from 'aframe-react';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import SkyCity from "./images/SkyCity.jpg"
+import Room from "./images/Room.jpg"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  
+  changeScene() {
+    var back = document.querySelector("#background");
+    console.log(back);
+    console.log(back.getAttribute('src'));
+    
+    if (back.getAttribute('src') === "#city")
+    {
+      back.setAttribute('src', "#room");
+    }
+    else
+    {
+      back.setAttribute('src', "#city");
+    }
+  }
+  
+  render () {
+    return (
+      <Scene>
+        <a-assets>
+          <img id="city" src={SkyCity}/>
+          <img id="room" src={Room}/>
+        </a-assets>
+        
+        <Entity id="background" primitive="a-sky" radius="50" height="865" width="1786" src="#room"/>
+        <Entity id="box"
+          geometry={{primitive: 'box'}}
+          position={{x: 0, y: 1, z: -3}}
+          events={{click: this.changeScene.bind(this)}}>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        </Entity>
+          
+        <Entity primitive="a-camera">
+          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+        </Entity>
+        
+      </Scene>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#sceneContainer'));
